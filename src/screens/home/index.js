4a3d76dd7bf70e0b16cda61e265c8e2c9a048ed7
felri <felipercmelo@gmail.com>
2021-03-06 21:28:29 -0300

    delete && like functions

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -11,6 +11,24 @@ function Home() {
     setUsers(data);
   }
 
+  function onLike({ id }) {
+    let aux = [...users];
+    const index = aux.map((f) => f.id).indexOf(id);
+    if (index > -1) {
+      aux[index].liked = aux[index].liked ? false : true;
+      setUsers(aux);
+    }
+  }
+
+  function onDelete({ id }) {
+    let aux = [...users];
+    const index = aux.map((f) => f.id).indexOf(id);
+    if (index > -1) {
+      aux.splice(index, 1);
+      setUsers(aux);
+    }
+  }
+
   useEffect(() => {
     getUsers();
   }, []);
@@ -18,7 +36,14 @@ function Home() {
   return (
     <div className='container-home'>
       {users.length > 0 &&
-        users.map((user) => <Card user={user} key={user.id} />)}
+        users.map((user) => (
+          <Card
+            onLike={() => onLike({ id: user.id })}
+            onDelete={() => onDelete({ id: user.id })}
+            user={user}
+            key={user.id}
+          />
+        ))}
     </div>
   );
 }
